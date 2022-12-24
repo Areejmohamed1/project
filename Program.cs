ff1da1bef6b17ee9477f9099f4a02c63f7645231
Areejmohamed1 <120640226+Areejmohamed1@users.noreply.github.com>
2022-12-24 13:05:05 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-for (int i = 5; i < 30; i++)
-{
-    int perfectnum = 0;
-    for (int j = 1; j < i; j++)
-    {
-        if (i % j == 0)
-        {
-            perfectnum = perfectnum + j;
-        }
-    }
-    if (perfectnum == i)
-        Console.WriteLine("the perfect numbers is" + perfectnum);
-}
